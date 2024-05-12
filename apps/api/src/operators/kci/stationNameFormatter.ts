ab864fd38468b98b1bb1468e19b400cfb3c653e8
Dhika Rizky <deka_r@outlook.com>
2024-05-12 13:03:43 +0700

    Improve formatter

--- apps/api/src/operators/kci/stationNameFormatter.ts
+++ apps/api/src/operators/kci/stationNameFormatter.ts
@@ -3,12 +3,21 @@ const WELL_KNOWN_STATION_NAMES: Record<string, string> = {
   KLDB: "Klender Baru",
   GST: "Gang Sentiong",
   DRN: "Duren Kalibata",
-  LNA: "Lenteng Agung"
+  LNA: "Lenteng Agung",
+  PSM: "Pasar Minggu",
+  PSMB: "Pasar Minggu Baru"
 }
 
 export function tryGetFormattedName(code: string, stationName: string) {
   const wellKnownName = WELL_KNOWN_STATION_NAMES[code]
   if (wellKnownName) return wellKnownName
 
-  return `${stationName[0]}${stationName.toLowerCase().substring(1)}`
+  // Return station name with capitalized each word name
+  return stationName.split(/[ .]/g)
+    .map((word) => {
+      if (word === 'UNIV') return 'Universitas'
+      return `${word[0]}${word.toLowerCase().substring(1)}`}
+    )
+    .join(" ")
+    .trim()
 }
