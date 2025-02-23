14b21217355af15b2e908f48076606d14dc9d38b
Dhika Rizky <deka_r@outlook.com>
2025-02-24 01:35:17 +0700

    Make search page functional

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,14 +1,22 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 
 import kciRoutes from './operators/kci/routes'
 import mrtjRoutes from './operators/mrtj/routes'
+import { StationRepository } from 'db/repositories/stations'
+import { Ok } from 'utils/response'
 
 const PORT = process.env.PORT ? Number.parseInt(process.env.PORT) : 3000
 const app = new Hono()
 
+app.use('*', cors())
 app.route('kci', kciRoutes)
 app.route('mrtj', mrtjRoutes)
+app.get('/stations', async (c) => {
+  const stations = await StationRepository.getAll()
+  return c.json(Ok(stations), 200)
+})
 
 serve({
   fetch: app.fetch,
--- apps/web/app/routes/add-station.tsx
+++ apps/web/app/routes/add-station.tsx
@@ -1,17 +1,53 @@
-export async function clientLoader() {
+import type { Station } from '@schema/stations'
+import type { StandardResponse } from '@schema/response'
+import type { Route } from './+types/add-station'
+import { useMemo, useState } from 'react'
 
+export async function clientLoader(): Promise<StandardResponse<Station[]>> {
+  const stations = await fetch(new URL('/stations', import.meta.env.VITE_API_BASE_URL))
+  if (stations.ok) return await stations.json()
+  return {
+    status: 200,
+    data: []
+  }
 }
 
-export default function AddStation() {
+export default function AddStation({ loaderData }: Route.ComponentProps) {
+  const stations = loaderData
+  const [searchQuery, setSearchQuery] = useState<string>("")
+
+  const filteredStations = useMemo(() => {
+    if (stations.data === undefined || searchQuery.length < 2) return []
+    return stations.data.filter(
+      station =>
+        station.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        station.formattedName?.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        station.code.toLowerCase().includes(searchQuery.toLowerCase())
+    )
+  }, [searchQuery])
+
   return (
-    <div className="p-4">
-      <h2 className="font-bold text-2xl">Tambahkan Stasiun</h2>
-      <ul className="mt-4">
-        <li><b>Commuter Line</b></li>
-
-        <li className="mt-2"><b>MRT Jakarta</b></li>
-
-      </ul>
+    <div>
+      <div className="p-8 pb-0">
+        <h1 className="font-bold text-2xl">Cari Stasiun</h1>
+        <input
+          className="mt-4 w-full px-4 py-2 rounded bg-stone-200"
+          type="text"
+          placeholder="Masukkan nama stasiun atau kode stasiun"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+      </div>
+      {filteredStations.length > 0 ? (
+        <ul className="mt-8">
+          {filteredStations.map(station => (
+              <li className="px-8 py-4 flex flex-col gap-1">
+                <b>{ station.formattedName }</b>
+                <span className="font-semibold">{ station.operator }</span>
+              </li>
+            ))}
+        </ul>
+      ) : null}
     </div>
   )
 }
