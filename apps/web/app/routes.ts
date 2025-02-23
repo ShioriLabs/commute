39557dd2832ced1e3f4b764ed16887547a54a1a7
Dhika Rizky <deka_r@outlook.com>
2025-02-24 03:08:46 +0700

    Update search page

--- apps/web/app/routes.ts
+++ apps/web/app/routes.ts
@@ -3,6 +3,6 @@ import { type RouteConfig, index, layout, route } from "@react-router/dev/routes
 export default [
   layout('layouts/default.tsx', [
     index('routes/home.tsx'),
-    route('add-station', 'routes/add-station.tsx')
+    route('search', 'routes/search.tsx')
   ])
 ] satisfies RouteConfig
