a31b3db135c58b4a38a9d881aad37dc726a19e9f
Dhika Rizky <deka_r@outlook.com>
2025-02-24 03:11:42 +0700

    Update search button radius

--- apps/web/app/routes/home.tsx
+++ apps/web/app/routes/home.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   return (
     <main className="bg-gray-100 w-screen min-h-screen">
       <nav className="fixed bottom-0 py-4 flex gap-4">
-        <Link to="/search" className="ml-4 bg-white block p-4 rounded-md shadow w-screen h-screen max-w-40 max-h-28 border-2 border-gray-200">
+        <Link to="/search" className="ml-4 bg-white block p-4 rounded-xl shadow w-screen h-screen max-w-40 max-h-28 border-2 border-gray-200">
           <b>Cari</b>
         </Link>
       </nav>
