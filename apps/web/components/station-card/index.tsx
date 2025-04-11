04c8316758117ed7caf89046c78d0a806a17c3a5
Dhika Rizky <deka_r@outlook.com>
2025-04-12 00:43:13 +0700

    Extracted station cards into StationCard component

---
+++ apps/web/components/station-card/index.tsx
@@ -0,0 +1,51 @@
+import type { LineTimetable, Schedule } from 'models/schedules'
+
+function getNextSchedules(schedules: Schedule[], limit = 3) {
+  const now = new Date()
+  const returning: Schedule[] = []
+  for (const schedule of schedules) {
+    if (returning.length === limit) break
+    const parsedDeparture = new Date(schedule.estimatedDeparture)
+    if (parsedDeparture < now) continue
+    returning.push(schedule)
+  }
+
+  return returning
+}
+
+function parseTime(timeString: string) {
+  return new Date(`${new Date().toDateString()} ${timeString}`)
+}
+
+interface Props {
+  line: LineTimetable
+}
+
+export default function StationCard({ line }: Props) {
+  return (
+    <li className="rounded-lg w-full min-h-8 shadow-lg border-t-[16px] border-gray-100" style={{ borderTopColor: line.colorCode }}>
+      <article className="p-4">
+        <h1 className="font-bold text-xl">{line.name}</h1>
+      </article>
+      <ul className="border-t border-t-gray-300">
+        {line.timetable.map(direction => {
+          const nextSchedules = getNextSchedules(direction.schedules)
+
+          return (
+            <li key={direction.boundFor} className="p-4 flex items-start justify-between gap-2">
+              <div>
+                <span className="font-bold">{direction.boundFor}</span>
+              </div>
+              <div className="text-right flex flex-col">
+                <span className="font-bold">{parseTime(nextSchedules[0].estimatedDeparture).toLocaleTimeString('id-ID', { timeStyle: 'short' })}</span>
+                {nextSchedules.length > 1 ? (
+                  <span className="font-bold text-sm text-gray-500">lalu {nextSchedules.slice(1, 3).map(sched => parseTime(sched.estimatedDeparture).toLocaleTimeString('id-ID', { timeStyle: 'short' })).join(', ')}</span>
+                ) : null}
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+    </li>
+  )
+}
