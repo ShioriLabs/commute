106d91e32c32a8fcc7fbc24f3a9bb47a5d7fcdef
Dhika Rizky <deka_r@outlook.com>
2025-04-17 22:13:28 +0700

    Add tinting to line card

--- apps/web/app/components/line-card/index.tsx
+++ apps/web/app/components/line-card/index.tsx
@@ -17,13 +17,31 @@ function parseTime(timeString: string) {
   return new Date(`${new Date().toDateString()} ${timeString}`)
 }
 
+function tintHex(hex: string, tintFactor = 0.2, towards: 'light' | 'dark' = 'light') {
+  hex = hex.replace(/^#/, "")
+
+  let r = parseInt(hex.substring(0, 2), 16)
+  let g = parseInt(hex.substring(2, 4), 16)
+  let b = parseInt(hex.substring(4, 6), 16)
+
+  const target = towards === 'light' ? 255 : 128
+
+  r = Math.round(r * tintFactor + target * (1 - tintFactor))
+  g = Math.round(g * tintFactor + target * (1 - tintFactor))
+  b = Math.round(b * tintFactor + target * (1 - tintFactor))
+
+  return "#" + [r, g, b].map(x =>
+    x.toString(16).padStart(2, "0")
+  ).join("")
+}
+
 interface Props {
   line: LineTimetable
 }
 
 export default function LineCard({ line }: Props) {
   return (
-    <li className="rounded-lg w-full min-h-8 shadow-lg border-t-[16px] border-gray-100" style={{ borderTopColor: line.colorCode }}>
+    <li className="rounded-lg w-full min-h-8 shadow-lg border-t-[16px] border-gray-100" style={{ borderTopColor: line.colorCode, backgroundColor: tintHex(line.colorCode, 0.075) }}>
       <article className="p-4">
         <h1 className="font-bold text-xl">{line.name}</h1>
       </article>
@@ -33,7 +51,7 @@ export default function LineCard({ line }: Props) {
           if (nextSchedules.length === 0) return null
 
           return (
-            <li key={direction.boundFor} className="p-4 flex items-start justify-between border-t border-t-gray-300">
+            <li key={direction.boundFor} className="p-4 flex items-start justify-between border-t-2" style={{ borderTopColor: tintHex(line.colorCode, 0.4) }}>
               <div>
                 <span className="font-bold">{direction.boundFor}</span>
               </div>
