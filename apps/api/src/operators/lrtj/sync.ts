3ae4547520ed783a39547d5d9a3acf863cb32a4f
Dhika Rizky Agusta <deka_r@outlook.com>
2025-05-22 14:52:18 +0700

    Add LRTJ support

--- apps/api/src/operators/lrtj/sync.ts
+++ apps/api/src/operators/lrtj/sync.ts
@@ -3,6 +3,7 @@ import { StationRepository } from 'db/repositories/stations'
 import { NewStation } from 'db/schemas/stations'
 import { chunkArray } from 'utils/chunk'
 import { parseHTML } from 'linkedom'
+import { NewSchedule } from 'db/schemas/schedules'
 
 export async function syncStations(d1: D1Database) {
   const response = await fetch('https://www.lrtjakarta.co.id/jadwal.html')
@@ -41,4 +42,79 @@ export async function syncStations(d1: D1Database) {
   }
 
   return stations
+}
+
+async function fetchTimetable(originStationId: number, direction: 'PGDBOUND' | 'VELBOUND') {
+  const times: string[] = []
+
+  let destinationStationId = ''
+  if (direction === 'PGDBOUND') destinationStationId = '6'
+  if (direction === 'VELBOUND') destinationStationId = '1'
+
+  const response = await fetch('https://www.lrtjakarta.co.id/jadwal.html', {
+    body: new URLSearchParams({
+      stasiun_awal: originStationId.toString(),
+      stasiun_akhir: destinationStationId,
+      search_jadwal: ''
+    }),
+    method: 'POST'
+  })
+
+  if (!response.ok || response.status !== 200) {
+    return []
+  }
+
+  const rawText = await response.text()
+  const { document } = parseHTML(rawText)
+
+  document.querySelectorAll('.list-time .item-time').forEach((time) => {
+    const timeText = time.textContent
+    if (timeText) times.push(timeText)
+  })
+
+  return times
+}
+
+export async function syncTimetable(d1: D1Database, stationCode: string) {
+  const stationCodeObject = Object.entries(LRTJ_STATION_CODES).find(([_, value]) => value === stationCode)
+  if (!stationCodeObject) return []
+
+  const originalStationId = Number.parseInt(stationCodeObject[0])
+  const timetable: NewSchedule[] = []
+
+  const stationId = `${OPERATORS.LRTJ.code}-${stationCode}`
+
+  if (originalStationId < 6) {
+    const times = await fetchTimetable(originalStationId, 'PGDBOUND')
+    for (const time of times) {
+      timetable.push({
+        id: `${stationId}-${time}-PGD-BOUND`,
+        boundFor: 'Pegangsaan Dua',
+        estimatedDeparture: `${time}:00`,
+        estimatedArrival: '00:00:00', // currently unused, TODO calculate by predefined timing
+        lineCode: 'S',
+        stationId: stationId,
+        tripNumber: `${stationId}-${time}-PGD-BOUND`,
+      })
+    }
+  }
+  if (originalStationId > 1) {
+    const times = await fetchTimetable(originalStationId, 'VELBOUND')
+    for (const time of times) {
+      timetable.push({
+        id: `${stationId}-${time}-VEL-BOUND`,
+        boundFor: 'Velodrome',
+        estimatedDeparture: `${time}:00`,
+        estimatedArrival: '00:00:00', // currently unused, TODO calculate by predefined timing
+        lineCode: 'S',
+        stationId: stationId,
+        tripNumber: `${stationId}-${time}-PGD-BOUND`,
+      })
+    }
+  }
+
+  // Save to database
+  await new StationRepository(d1).insertTimetable(stationId, timetable)
+
+  return timetable
 }
