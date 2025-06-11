24aaadc1b0bb1e2f0dc126207d4d04ff606beaf8
Dhika Rizky Agusta <deka_r@outlook.com>
2025-06-11 18:20:10 +0700

    Unify operator data APIs

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -4,9 +4,7 @@ import { cors } from 'hono/cors'
 import kciRoutes from './operators/kci/routes'
 import mrtjRoutes from './operators/mrtj/routes'
 import lrtjRoutes from './operators/lrtj/routes'
-import { StationRepository } from 'db/repositories/stations'
-import { Ok } from 'utils/response'
-import { KVRepository } from 'db/repositories/kv'
+import stations from './routes/stations'
 
 export interface Bindings {
   DB: D1Database
@@ -21,35 +19,6 @@ app.use('*', cors())
 app.route('KCI', kciRoutes)
 app.route('MRTJ', mrtjRoutes)
 app.route('LRTJ', lrtjRoutes)
-app.get('/stations', async (c) => {
-  const kvRepository = new KVRepository(c.env.KV)
-  const stationRepository = new StationRepository(c.env.DB)
-
-  const kvKey = `stations_${c.env.API_VERSION}`
-  const stations = await kvRepository.get(kvKey)
-  if (stations) {
-    return c.json(
-      Ok(stations),
-      200
-    )
-  }
-
-  const freshStations = await stationRepository.getAll()
-  if (!freshStations || freshStations.length === 0) {
-    return c.json(
-      Ok([]),
-      200
-    )
-  }
-
-  await kvRepository.set(kvKey, JSON.stringify(freshStations))
-
-  return c.json(
-    Ok(
-      freshStations
-    ),
-    200
-  )
-})
+app.route('stations', stations)
 
 export default app
