64a63b5d125272ba8c63ee13e66322fd0ccce227
Dhika Rizky <deka_r@outlook.com>
2025-06-14 17:29:16 +0700

    Add line info in operators info

--- apps/api/src/routes/operators.ts
+++ apps/api/src/routes/operators.ts
@@ -2,12 +2,19 @@ import { Hono } from 'hono'
 import { Bindings } from 'app'
 import { OPERATORS } from '@commute/constants'
 import { Ok } from 'utils/response'
+import { ALL_LINES } from 'utils/line'
 
 const app = new Hono<{ Bindings: Bindings }>()
 
 app.get('/', async (c) => {
   const operators = Object.values(OPERATORS)
     .filter(op => op.code !== 'NUL')
+    .map((op) => {
+      return {
+        ...op,
+        lines: ALL_LINES[op.code]
+      }
+    })
 
   return c.json(
     Ok(operators),
